bbd32fcc4f262e77c755d877231c7874c7ebc172
dev-farrukh <m.farrukhnoman@gmail.com>
2026-09-23 03:41:08 -0700

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Router from "./Router";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Router />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
